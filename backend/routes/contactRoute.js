aa0d90b661a279eb3051fba8fd3a96b90a371b3a
Mr Rahul <ashish227087@gmail.com>
2026-02-04 00:30:54 +0530

    now version 2.1

--- backend/routes/contactRoute.js
+++ backend/routes/contactRoute.js
@@ -16,12 +16,10 @@ router.post("/", async (req, res) => {
       message,
     });
 
+
     // 2. Mail transporter
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-
+      service:"gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
